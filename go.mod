bf15e53cca18970562ada0d5b01afbbe9275aa4a
Andi <ic20b050@technikum-wien.at>
2023-02-27 00:58:23 +0100

    add db connection

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module Bac
+
+go 1.19
+
+require github.com/lib/pq v1.10.7 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
+// Sources:
+// https://go.dev/doc/tutorial/web-service-gin
+// https://golangdocs.com/golang-postgresql-example
+
 package main
 
 import (
+	"database/sql"
 	//"encoding/json"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"strings"
+)
+
+const (
+	host = "127.0.0.1"
+	// host = "192.168.0.2" // container ip
+	port     = 5432
+	user     = "postgres"
+	password = "xsmmsgbAMfIOIWPPBrsc"
+	database = "ogd"
 )
 
 func returnHello(w http.ResponseWriter, r *http.Request) {
@@ -34,5 +49,45 @@ func main() {
 	fmt.Println("http://localhost:9000/")
 	fmt.Println("GET: http://localhost:9000/hello?name=ic20b050")
 
-	handleRequests()
+	//handleRequests()
+
+	// connection string
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, database)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// close database
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+	selectFromDB(db)
+}
+
+func selectFromDB(db *sql.DB) {
+	rows, err := db.Query(`SELECT gemeindename FROM public.gemeinde WHERE gkz=10101 LIMIT 1`)
+	CheckError(err)
+
+	defer rows.Close()
+	for rows.Next() {
+		var name string
+
+		err = rows.Scan(&name)
+		CheckError(err)
+
+		fmt.Println(name)
+	}
+
+	CheckError(err)
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
