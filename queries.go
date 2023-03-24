5a93ea52b4e6b0b59f489a95900f6ef8c48d24a8
Andi <ic20b050@technikum-wien.at>
2023-03-24 19:35:18 +0100

    Code aufgeräumt

--- queries.go
+++ queries.go
@@ -9,34 +9,9 @@ type jsonMessage struct {
 	Message string `json:"message"`
 }
 
-type jsonName struct {
-	Name string `json:"name"`
-}
-
 func home(c *gin.Context) {
 	var response = jsonMessage{
 		Message: "Hello World! I'm the Go API.",
-	}
-
-	c.IndentedJSON(http.StatusOK, response)
-}
-
-func sayHelloGet(c *gin.Context) {
-	name := c.Query("name")
-
-	var response = jsonMessage{
-		Message: "Hello " + name + "! I'm the Go API.",
-	}
-
-	c.IndentedJSON(http.StatusOK, response)
-}
-
-func sayHelloPost(c *gin.Context) {
-	var name jsonName
-	c.BindJSON(&name)
-
-	var response = jsonMessage{
-		Message: "Hello " + name.Name + "! I'm the Go API.",
 	}
 
 	c.IndentedJSON(http.StatusOK, response)
