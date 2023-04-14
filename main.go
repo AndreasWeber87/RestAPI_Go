9f385e145825cd4068b498e7f589229f77ce1eec
Andi <ic20b050@technikum-wien.at>
2023-04-15 00:43:23 +0200

    finished crud

--- main.go
+++ main.go
@@ -17,12 +17,20 @@ func main() {
 	fmt.Println("")
 	fmt.Println("Possible calls:")
 	fmt.Println("http://localhost:7000/")
-	fmt.Println("GET: http://localhost:7000/getGemeinde?id=10101")
+	fmt.Println("POST: http://localhost:7000/createTable")
+	fmt.Println("POST: http://localhost:7000/addGemeinde   gkz=10101 gemeindename=Eisenstadt")
+	fmt.Println("PUT: http://localhost:7000/changeGemeinde   gkz=10101 gemeindename=Eisenstadt2")
+	fmt.Println("GET: http://localhost:7000/getGemeinde?gkz=10101")
+	fmt.Println("DELETE: http://localhost:7000/deleteGemeinde/10101")
 
 	router := gin.Default()
 	router.GET("/", home)
 
+	router.POST("/createTable", createTable)
+	router.POST("/addGemeinde", addGemeinde)
+	router.PUT("/changeGemeinde", changeGemeinde)
 	router.GET("/getGemeinde", getGemeinde)
+	router.DELETE("/deleteGemeinde/:gkz", deleteGemeinde)
 
 	err := router.Run(":10000")
 	checkError(err)
--- queriesDB.go
+++ queriesDB.go
@@ -4,31 +4,96 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"net/http"
+	"strconv"
 )
 
 type jsonGemeinde struct {
+	GKZ          int    `json:"gkz"`
 	Gemeindename string `json:"gemeindename"`
 }
 
 var dbConn = connectDB()
 
+func createTable(c *gin.Context) {
+	var sqlQuery = `DROP TABLE IF EXISTS public.gemeinde;
+
+CREATE TABLE IF NOT EXISTS public.gemeinde
+(
+    gkz integer NOT NULL,
+    gemeindename character varying(50) COLLATE pg_catalog."default",
+    CONSTRAINT gemeinde_pkey PRIMARY KEY (gkz)
+)`
+	_, err := dbConn.Query(sqlQuery)
+	checkError(err)
+
+	c.Status(http.StatusCreated)
+}
+
+func addGemeinde(c *gin.Context) {
+	var gemeinde jsonGemeinde
+
+	err := c.BindJSON(&gemeinde)
+	checkError(err)
+
+	var sqlQuery = "INSERT INTO public.gemeinde(gkz, gemeindename) VALUES ($1, $2);"
+	stmt, err := dbConn.Prepare(sqlQuery)
+	checkError(err)
+
+	_, err = stmt.Query(gemeinde.GKZ, gemeinde.Gemeindename)
+	checkError(err)
+
+	c.Status(http.StatusCreated)
+}
+
+func changeGemeinde(c *gin.Context) {
+	var gemeinde jsonGemeinde
+
+	err := c.BindJSON(&gemeinde)
+	checkError(err)
+
+	var sqlQuery = "UPDATE public.gemeinde SET gemeindename=$1 WHERE gkz=$2;"
+	stmt, err := dbConn.Prepare(sqlQuery)
+	checkError(err)
+
+	_, err = stmt.Query(gemeinde.Gemeindename, gemeinde.GKZ)
+	checkError(err)
+
+	c.Status(http.StatusOK)
+}
+
 func getGemeinde(c *gin.Context) {
-	var response []jsonGemeinde
-	var id = c.Query("id")
-
-	stmt, err := dbConn.Prepare("SELECT gemeindename FROM public.gemeinde WHERE gkz=$1 LIMIT 1")
-	checkError(err)
-	rows, err := stmt.Query(id)
+	gkz, err := strconv.Atoi(c.Query("gkz"))
 	checkError(err)
 
-	for rows.Next() {
-		var gemeinde string
+	var sqlQuery = "SELECT gemeindename FROM public.gemeinde WHERE gkz=$1 LIMIT 1"
+	stmt, err := dbConn.Prepare(sqlQuery)
+	checkError(err)
 
-		err = rows.Scan(&gemeinde)
-		checkError(err)
+	rows, err := stmt.Query(gkz)
+	checkError(err)
 
-		response = append(response, jsonGemeinde{Gemeindename: gemeinde})
+	rows.Next()
+	var gemeindename string
+	err = rows.Scan(&gemeindename)
+
+	if err != nil {
+		c.Status(http.StatusNotFound)
+		return
 	}
 
-	c.IndentedJSON(http.StatusOK, response)
+	c.IndentedJSON(http.StatusOK, jsonGemeinde{GKZ: gkz, Gemeindename: gemeindename})
+}
+
+func deleteGemeinde(c *gin.Context) {
+	gkz, err := strconv.Atoi(c.Param("gkz"))
+	checkError(err)
+
+	var sqlQuery = "DELETE FROM public.gemeinde WHERE gkz=$1;"
+	stmt, err := dbConn.Prepare(sqlQuery)
+	checkError(err)
+
+	_, err = stmt.Query(gkz)
+	checkError(err)
+
+	c.Status(http.StatusOK)
 }
